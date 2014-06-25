ca594c9dbc8a6f555b90685c6ea2ca339871e5c3
Brian Broll <brian.broll@gmail.com>
2014-06-25 15:01:51 -0500

    Updated the rating msg box message

--- app/src/main/java/com/gmail/brian/broll/taxidash/app/TaxiDash.java
+++ app/src/main/java/com/gmail/brian/broll/taxidash/app/TaxiDash.java
@@ -1,28 +1,44 @@
 package com.gmail.brian.broll.taxidash.app;
 
-import com.gmail.brian.broll.taxidash.app.util.SystemUiHider;
 
-import android.annotation.TargetApi;
+import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
+import android.os.Handler;
 
 
 /**
  * Splash screen for the TaxiDash app. Here we initially load the server info.
  */
 public class TaxiDash extends Activity {
+    private static int CAN_PROCEED = 2;
+    private static long MIN_SPLASH_TIME = 2400;
+    private int currentStatus = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getActionBar().hide();
+        ActionBar actionBar = getActionBar();
+
+        if(actionBar != null) {
+            actionBar.hide();
+        }
 
         setContentView(R.layout.activity_taxi_dash);
 
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if(++currentStatus == CAN_PROCEED) {
+                    startNearbyCabs();
+                }
+            }
+        }, MIN_SPLASH_TIME);
+
         new InitializeConstants().execute();
     }
+
     private class InitializeConstants extends Utils.setTaxiDashConstants {
 
         @Override
@@ -33,13 +49,19 @@ public class TaxiDash extends Activity {
         @Override
         protected void onPostExecute(Void result) {
             super.onPostExecute(result);
-            // After completing http call
-            // will close this activity and lauch main activity
-            Intent i = new Intent(TaxiDash.this, NearbyCabList.class);
-            startActivity(i);
-            // close this activity
-            finish();
+            if(++currentStatus == CAN_PROCEED) {
+                startNearbyCabs();
+            }
         }
+    }
+
+    private void startNearbyCabs(){
+        // After completing http call
+        // will close this activity and lauch main activity
+        Intent i = new Intent(TaxiDash.this, NearbyCabList.class);
+        startActivity(i);
+        // close this activity
+        finish();
     }
 }
 
